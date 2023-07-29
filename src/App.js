f3ad993587eaef9abc68fdf0f8c2b7de503c37b0
jandrusak <jonathanandrusak@gmail.com>
2023-07-28 21:27:08 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  //new app
+  constructor(props) {
+    // React.Component. contstructor(props);
+    super(props);
+    this.state = {
+      // isClicked: false,
+      todos: [],
+      text: ''
+    };
+    this.onClickHandler = () => {
+      const text = this.state.text;
+      const todos = [...this.state.todos, text];
+      this.setState({
+        todos: todos,
+        text: ''
+      });
+    };
+
+    this.onChangeHandler = (e) => {
+      this.setState({
+        text: e.target.value
+      })
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello World!
+        <hr />
+        <button onClick={this.onClickHandler}> 
+        Add Item
+        </button>
+          <input type="text" 
+          value ={this.state.text}
+          onChange={this.onChangeHandler} />
+          <h3>ToDo List</h3>
+          <ul>
+            {this.state.todos.map((todo, index) => {
+              return (
+                <Todo key={index}> {todo} </Todo>
+              );
+              })}
+              </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+class Todo extends React.Component {
+  render() {
+    return (
+      <li className="Todo">
+        <input type="checkbox" /> {this.props.children}
+       </li>
+    )
+  }
+}
+
+
+
